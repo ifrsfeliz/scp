64b8fba5f56dd1d43495093a3200bab39da3ed44
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2015-03-20 17:19:47 -0300

    Removed bin from shared links and set them executable

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/ticpfeliz/app/scp'
 set :linked_files, %w{config/database.yml config/secrets.yml config/settings.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/files}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/files}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
