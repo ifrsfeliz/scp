bfaa04f10eecf5dfa2df2c59f66cfc7e640c9c21
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2015-11-25 14:37:21 -0200

    Fix migration

--- db/migrate/20150324192430_change_all_columns_references_to_professor_to_researcher.rb
+++ db/migrate/20150324192430_change_all_columns_references_to_professor_to_researcher.rb
@@ -1,17 +1,13 @@
 class ChangeAllColumnsReferencesToProfessorToResearcher < ActiveRecord::Migration
   def change
-    remove_column :documents, :professor_id
-    add_column :documents, :researcher_id, :integer
+    rename_column :documents, :professor_id, :researcher_id
 
     rename_table :member_professors, :member_researchers
 
-    remove_column :member_researchers, :professor_id
-    add_column :member_researchers, :researcher_id, :integer
+    rename_column :member_researchers, :professor_id, :researcher_id
 
-    remove_column :projects, :professor_id
-    add_column :projects, :professor_id, :integer
+    rename_column :projects, :professor_id, :researcher_id
 
-    remove_column :research_groups, :professor_id
-    add_column :research_groups, :professor_id, :integer
+    rename_column :research_groups, :professor_id, :researcher_id
   end
 end
