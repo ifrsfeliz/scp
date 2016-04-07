f0f8d900c7fab47b650ac2a626a832d0fc491aa5
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2016-04-07 12:59:23 -0300

    Test save_and_open_page on codeship

--- spec/support/feature_macros.rb
+++ spec/support/feature_macros.rb
@@ -2,6 +2,8 @@ module FeatureMacros
   def sign_in_as_admin(admin)
     visit new_user_session_path
 
+    save_and_open_page
+
     fill_in 'user_email',    with: admin.email
     fill_in 'user_password', with: admin.password
 
