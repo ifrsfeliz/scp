99b96616db6a693e1a5ee505e8409e42582c5faf
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2016-04-07 12:17:55 -0300

    changed spec test to use id instead of name attribute

--- spec/support/feature_macros.rb
+++ spec/support/feature_macros.rb
@@ -1,8 +1,8 @@
 module FeatureMacros
   def sign_in_as_admin(admin)
     visit new_user_session_path
-    fill_in 'user[email]',    with: admin.email
-    fill_in 'user[password]', with: admin.password
+    fill_in 'user_email',    with: admin.email
+    fill_in 'user_password', with: admin.password
 
     click_button 'Log in'
   end
