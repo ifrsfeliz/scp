1d462144b8486436cd4af36d56c702f70c838b48
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2016-04-11 10:03:57 -0300

    Setting max time wait to capybara, to check if will solve problem on codeship

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,8 @@ require 'capybara/rails'
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 10
+
 RSpec.configure do |config|
   config.include Capybara::DSL, type: :request
 end
